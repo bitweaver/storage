7ad1af840f812a7d687749e37a6e92b8d11d7777
Max Kremmel <xing@synapse.plus.com>
2006-11-25 16:22:52 +0000

    make storage a required package and add an icon

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,6 +4,7 @@ global $gBitSystem, $gBitUser ;
 $registerHash = array(
 	'package_name' => 'storage',
 	'package_path' => dirname( __FILE__ ).'/',
+	'required_package'=> TRUE,
 );
 $gBitSystem->registerPackage( $registerHash );
 
