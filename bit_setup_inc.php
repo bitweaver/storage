67a39e50287e1942b561f567908624afc8fee2e4
Christian Fowler <spider@viovio.com>
2007-09-22 20:24:24 +0000

    add default STORAGE_HOST_URI for optionally splitting off storage files to separate host

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -8,4 +8,12 @@ $registerHash = array(
 );
 $gBitSystem->registerPackage( $registerHash );
 
+
+// add default STORAGE_HOST_URI for optionally splitting off storage files to separate host
+// to serve thumbnails and other files from a different server from your web server, add similar to your kernel/config_inc.php
+// define( 'STORAGE_HOST_URI', 'http://myfiles.example.com' ); or whatever your storage host name is...
+if( !defined( 'STORAGE_HOST_URI' ) ) {
+	define( 'STORAGE_HOST_URI', BIT_ROOT_URI );
+}
+
 ?>
