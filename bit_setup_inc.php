b2bbf5deb460f057a29659de167b3023a4067915
Max Kremmel <xing@synapse.plus.com>
2007-09-24 11:52:29 +0000

    set default BIT_ROOT_URI if not set, move STORAGE_HOST_URI and BIT_ROOT_URI definition to kernel/config_inc.php

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -8,10 +8,7 @@ $registerHash = array(
 );
 $gBitSystem->registerPackage( $registerHash );
 
-
-// add default STORAGE_HOST_URI for optionally splitting off storage files to separate host
-// to serve thumbnails and other files from a different server from your web server, add similar to your kernel/config_inc.php
-// define( 'STORAGE_HOST_URI', 'http://myfiles.example.com' ); or whatever your storage host name is...
+// set in kernel/config_inc.php
 if( !defined( 'STORAGE_HOST_URI' ) ) {
 	define( 'STORAGE_HOST_URI', BIT_ROOT_URI );
 }
