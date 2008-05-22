f8c1fa5d64a7903f4c566fcb38e62b8d190f8b3e
laetzer <laetzer@users.sourceforge.net>
2008-05-22 18:52:03 +0000

    package info for listings

---
+++ admin/schema_inc.php
@@ -0,0 +1,10 @@
+<?php
+
+global $gBitInstaller;
+
+$gBitInstaller->registerPackageInfo( STORAGE_PKG_NAME, array(
+	'description' => 'Directory to store information to the file system.',
+	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
+) );
+
+?>
